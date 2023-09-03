cf041909cccc341ee20a8a7a6f3b38feb308c986
Barbara Macedo <84219818+Barbara-sm@users.noreply.github.com>
2023-09-02 23:35:21 -0300

    Add files via upload
     Desafio Classificador de nível de Heró

---
+++ classificador_nivel_heroi/script.js
@@ -0,0 +1,21 @@
+
+let userName = 'Barbara';
+let xp = 6875;
+
+if (xp <= 1000) {
+    console.log(`O héroi de nome ${userName} está no nível Ferro`)
+} else if (xp >= 1001 && xp <= 2000) {
+    console.log(`O héroi de nome ${userName} está no nível Bronze`)
+} else if (xp >= 2001 && xp <= 5000) {
+    console.log(`O héroi de nome ${userName} está no nível Ouro`)
+} else if (xp >= 5001 && xp <= 7000) {
+    console.log(`O héroi de nome ${userName} está no nível Platina`)
+} else if (xp >= 7001 && xp <= 8000) {
+    console.log(`O héroi de nome ${userName} está no nível Ascendente`)
+} else if (xp >= 8001 && xp <= 9000) {
+    console.log(`O héroi de nome ${userName} está no nível Imortal`)
+} else {
+    console.log(`O héroi de nome ${userName} está no nível Radiante`)
+}
+
+
